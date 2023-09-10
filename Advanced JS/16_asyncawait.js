edf8c7ca74d7a6395a75b1bb16e1ac667eab8e93
codebyayush <ayushpal6939@gmail.com>
2023-09-10 18:46:57 +0530

    butterpopcorn

--- Advanced JS/16_asyncawait.js
+++ Advanced JS/16_asyncawait.js
@@ -8,13 +8,22 @@ const promiseWifeBringingTicks = new Promise((resolve, reject) => {
 })
 
 const getPopcorn = promiseWifeBringingTicks.then((t) => {
-    console.log(`Husband: We should go in`);
+    console.log(`Wife: I have the tickets.`);
+    console.log(`Husband: We should go in.`);
     console.log(`Wife: No I'm hungry.`);
     return new Promise((resolve, reject) => 
-    resolve(`-Walks in with ${t} and popcorn`))
+    resolve(`-Walks in with ${t}, popcorn`))
 });
 
-getPopcorn.then((t) => console.log(t))
+const getButter = getPopcorn.then((t) => {
+    console.log('Husband: I got some popcorn')
+    console.log(`Wife: I need butter on my popcorn`);
+    return new Promise((resolve, reject) => {
+        resolve(`${t} and butter`)
+    });
+});
+
+getButter.then((t) => console.log(t));
 
 console.log('person4: shows ticket')
 console.log('person5: shows ticket')
