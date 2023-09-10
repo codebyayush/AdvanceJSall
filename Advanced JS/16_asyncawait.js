778e6b5669d5546cd5a055fa6b2d221d8d67b23d
codebyayush <ayushpal6939@gmail.com>
2023-09-10 17:45:48 +0530

    resolve with popcorn

--- Advanced JS/16_asyncawait.js
+++ Advanced JS/16_asyncawait.js
@@ -7,9 +7,14 @@ const promiseWifeBringingTicks = new Promise((resolve, reject) => {
     }, 3000);
 })
 
-promiseWifeBringingTicks.then((t) => {
-    console.log(`person3: shows ${t}`)
+const getPopcorn = promiseWifeBringingTicks.then((t) => {
+    console.log(`Husband: We should go in`);
+    console.log(`Wife: No I'm hungry.`);
+    return new Promise((resolve, reject) => 
+    resolve(`-Walks in with ${t} and popcorn`))
 });
+
+getPopcorn.then((t) => console.log(t))
 
 console.log('person4: shows ticket')
 console.log('person5: shows ticket')
